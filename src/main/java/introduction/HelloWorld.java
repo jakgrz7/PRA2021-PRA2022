c5ef7702c953c72fbca2df7b1021250d96cac082
jakgrz7 <Paluszki2021>
2021-10-04 16:41:16 +0200

    commit 1

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,14 @@
 package introduction;
 
+import java.util.logging.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
         System.out.println("Hello! Witaj!");
+
+        Logger log = Logger.getLogger("name");
+
+        log.info("Dzień dobry!");
     }
 }
